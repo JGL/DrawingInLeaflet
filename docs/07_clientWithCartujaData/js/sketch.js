3399829ee7a13ee6c0ee4cd77aa1303448b9c1aa
JGL <me@joelgethinlewis.com>
2023-11-23 17:06:26 +0000

    Live location

--- docs/07_clientWithCartujaData/js/sketch.js
+++ docs/07_clientWithCartujaData/js/sketch.js
@@ -8,6 +8,7 @@ let tour;
 let theImages = [];
 let theImagesDisplay = [];
 let theImagesText = [];
+let theImagesPoly = [];
 
 function preload() {
 	//https://p5js.org/reference/#/p5/loadJSON
@@ -22,16 +23,20 @@ function preloadImagesFromGeoJSON() {
 	for (let index = 0; index < tour.features.length; index++) {
 		const urlForImage = tour.features[index].properties.multimedia;
 		const textForImage = tour.features[index].properties.name;
+		const polyForImage = tour.features[index].geometry.coordinates;
 		console.log(
-			`The ${index}, named  ${textForImage} polygon URL is ${urlForImage}.`
-		); //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals - use ` for the syntactic sugar!
+			`The ${index}, named  ${textForImage} polygon  ${polyForImage} is ${urlForImage}.`
+		);
+		//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals - use ` for the syntactic sugar!
 		//theImages.push(createImg(urlForImage, textForImage));
 		//https://p5js.org/reference/#/p5/createImg
 		theImages.push(loadImage(urlForImage));
 		theImagesDisplay.push(false);
 		theImagesText.push(textForImage);
+		theImagesPoly.push(polyForImage);
 	}
-	theImagesDisplay[8] = true;
+	//debugging
+	//theImagesDisplay[8] = true;
 }
 
 function windowResized() {
@@ -61,7 +66,7 @@ function setup() {
 	//console.log(canvas);
 	canvas.parent("drawing-area"); //https://github.com/processing/p5.js/wiki/Beyond-the-canvas
 
-	textSize(42); // 42 is the answer to everything
+	textSize(21); // 42/2 is the answer to everything
 	textAlign(CENTER, CENTER); //https://p5js.org/reference/#/p5/textAlign
 	frameRate(60);
 
@@ -93,6 +98,12 @@ function setup() {
 
 function draw() {
 	background(240);
+	text(
+		"Live position (lat,long) is: (" + latitude + ", " + longitude + ")",
+		width / 2,
+		100
+	);
+
 	for (let index = 0; index < theImagesDisplay.length; index++) {
 		if (theImagesDisplay[index]) {
 			image(theImages[index], width / 2, height / 2);
@@ -105,47 +116,19 @@ function positionChanged(position) {
 	print("lat: " + position.latitude);
 	print("long: " + position.longitude);
 
-	let closestRange = 9999999; //massive value to allow real ranges to beat this default value
-	let indexOfImageWithClosestRange = -1; //negative index to allow us to detect when we have a real index that is close enough
+	latitude = position.latitude;
+	longitude = position.longitude;
 
 	for (let index = 0; index < tour.features.length; index++) {
-		theImagesDisplay[index] = false; //default to displaying no images
+		// http://turfjs.org/docs/#booleanPointInPolygon
+		var pt = turf.point([position.latitude, position.longitude]);
+		var poly = turf.polygon(theImagesPoly[index]);
 
-		//TODO: turf.js fun here!
-		// const currentWaypointLatitude = Number(tour.waypoints[index].latitude);
-		// const currentWaypointLongitude = Number(tour.waypoints[index].longitude);
-		// const currentWaypointRange = Number(tour.waypoints[index].range);
-
-		// // BUG - I don't believe this is reporting an accurate position
-		// // https://github.com/bmoren/p5.geolocation#calcgeodistance
-		// let distanceFromWaypointToHere = calcGeoDistance(
-		//   position.latitude,
-		//   position.longitude,
-		//   currentWaypointLatitude,
-		//   currentWaypointLongitude,
-		//   "km"
-		// );
-		// distanceFromWaypointToHere *= 1000; //multiply distance by 1000 to get distance in metres
-
-		// console.log(
-		//   `The ${index} range from here ${distanceFromWaypointToHere}, the range of the waypoint is ${currentWaypointRange}.`
-		// );
-
-		// if (
-		//   distanceFromWaypointToHere < closestRange &&
-		//   distanceFromWaypointToHere < currentWaypointRange
-		// ) {
-		//   //then this is the closest waypoint so far in the array
-		//   //so set the closestRange to be distanceFromWaypointToHere
-		//   closestRange = distanceFromWaypointToHere;
-		//   indexOfImageWithClosestRange = index;
-		// }
+		if (turf.booleanPointInPolygon(pt, poly)) {
+			console.log(`The we are inside the ${index} polygon!`);
+			theImagesDisplay[index] = true;
+		} else {
+			theImagesDisplay[index] = false;
+		}
 	}
-
-	if (indexOfImageWithClosestRange > -1) {
-		//then we have a waypoint that is close enough in terms of range
-		theImagesDisplay[indexOfImageWithClosestRange] = true;
-	}
-
-	theImagesDisplay[8] = true;
 }
